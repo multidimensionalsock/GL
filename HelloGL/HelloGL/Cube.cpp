37cd971972cc6ae4d5205d969b0fd10b5f94725c
voidrobyn <92083124+voidrobyn@users.noreply.github.com>
2022-03-01 09:59:56 +0000

    task 2

--- HelloGL/HelloGL/Cube.cpp
+++ HelloGL/HelloGL/Cube.cpp
@@ -1,26 +1,18 @@
 #include "Cube.h"
 
-Vertex Cube::indexedVertices[] = { 1, 1, 1,  -1, 1, 1,  // v0,v1,
-				-1,-1, 1,   1,-1, 1,   // v2,v3
-				1,-1,-1,   1, 1,-1,    // v4,v5
-				-1, 1,-1,   -1,-1,-1 }; // v6,v7
-Color Cube::indexedColors[] = { 1, 1, 1,   1, 1, 0,   // v0,v1,
-				1, 0, 0,   1, 0, 1,   // v2,v3
-				0, 0, 1,   0, 1, 1,   // v4,v5
-				0, 1, 0,   0, 0, 0 }; //v6,v7
-
-GLushort Cube::indices[] = { 0, 1, 2,  2, 3, 0,      // front
-				0, 3, 4,  4, 5, 0,      // right
-				0, 5, 6,  6, 1, 0,      // top
-				1, 6, 7,  7, 2, 1,      // left
-				7, 4, 3,  3, 2, 7,      // bottom
-				4, 7, 6,  6, 5, 4 };    // back
+Vertex* Cube::indexedVertices = nullptr;
+Color* Cube::indexedColors = nullptr;
+GLushort* Cube::indices = nullptr;
+int Cube::numVertices = 0;
+int Cube::numColors = 0;
+int Cube::numIndices = 0;
 
 Cube::Cube(float x, float y, float z) {
 	_rotation = 0.0f;
 	_position.x = x;
 	_position.y = y;
 	_position.z = z;
+	Load((char*)"cube.txt");
 }
 
 Cube::~Cube() {
@@ -28,19 +20,22 @@ Cube::~Cube() {
 }
 
 void Cube::Draw() {
-	glTranslatef(_position.x, _position.y, _position.z);
-	glRotatef(_rotation, 1.0f, 0.0f, 0.0f);
-	glEnableClientState(GL_VERTEX_ARRAY);
-	glEnableClientState(GL_COLOR_ARRAY);
-	glVertexPointer(3, GL_FLOAT, 0, indexedVertices);
-	glColorPointer(3, GL_FLOAT, 0, indexedColors);
+	
+	if (indexedVertices != nullptr && indexedColors != nullptr && indices != nullptr) {
+		glTranslatef(_position.x, _position.y, _position.z);
+		glRotatef(_rotation, 1.0f, 0.0f, 0.0f);
+		glEnableClientState(GL_VERTEX_ARRAY);
+		glEnableClientState(GL_COLOR_ARRAY);
+		glVertexPointer(3, GL_FLOAT, 0, indexedVertices);
+		glColorPointer(3, GL_FLOAT, 0, indexedColors);
 
-	glPushMatrix();
-	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_SHORT, indices);
-	glPopMatrix();
+		glPushMatrix();
+		glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_SHORT, indices);
+		glPopMatrix();
 
-	glDisableClientState(GL_COLOR_ARRAY);
-	glDisableClientState(GL_VERTEX_ARRAY);
+		glDisableClientState(GL_COLOR_ARRAY);
+		glDisableClientState(GL_VERTEX_ARRAY);
+	}
 }
 
 void Cube::Update() {
@@ -49,4 +44,25 @@ void Cube::Update() {
 
 void Cube::SetRotation(float Rotation) {
 
+}
+
+bool Cube::Load(char* path) {
+	std::ifstream inFile;
+	inFile.open(path);
+	if (!inFile.good()) {
+		std::cerr << "Cant open text file" << path << std::endl;
+		return false;
+	}
+	inFile >> numVertices;
+	indexedVertices = new Vertex[numVertices];
+	std::cout << numVertices;
+	//for (int i = 0; i < numVertices; i++) {
+	//	std::getline(infile, 1)
+	//	//use infile to populate indexed array
+	//}
+	//for loop for colour load
+	//for loop for indices load
+
+	inFile.close();
+	return true;
 }
--- HelloGL/HelloGL/Cube.h
+++ HelloGL/HelloGL/Cube.h
@@ -5,12 +5,15 @@
 #include "GL\freeglut.h"
 #include "GLUTCallbacks.h"
 #include "Structures.h"
+#include <iostream>
+#include <fstream>
 
 class Cube{
 	private:
-		static Vertex indexedVertices[];
-		static Color indexedColors[];
-		static GLushort indices[]; // inbuilt typedef 16 bit unsigned int
+		static Vertex* indexedVertices;
+		static Color* indexedColors;
+		static GLushort* indices; // inbuilt typedef 16 bit unsigned int
+		static int numVertices, numColors, numIndices;
 		GLfloat _rotation;
 		Vector3 _position;
 
@@ -20,5 +23,6 @@ class Cube{
 		void Draw();
 		void Update();
 		void SetRotation(float Rotation);
+		static bool Load(char* path);
 };
 
