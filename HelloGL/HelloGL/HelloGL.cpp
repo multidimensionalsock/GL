1b3c1e9ac823a657d6b0cd6e1a9f8c1f2b2999aa
voidrobyn <92083124+voidrobyn@users.noreply.github.com>
2022-04-18 15:44:55 +0100

    textures added

--- HelloGL/HelloGL/HelloGL.cpp
+++ HelloGL/HelloGL/HelloGL.cpp
@@ -36,10 +36,10 @@ void HelloGL::Update() {
 	camera->up.x, camera->up.y, camera->up.z);
 	glTranslatef(0.0f, 0.0f, -5.0f);
 
-	glLightfv(GL_LIGHT0, GL_AMBIENT, &(_lightData->Ambient.x));
+	/*glLightfv(GL_LIGHT0, GL_AMBIENT, &(_lightData->Ambient.x));
 	glLightfv(GL_LIGHT0, GL_DIFFUSE, &(_lightData->Diffuse.x));
 	glLightfv(GL_LIGHT0, GL_DIFFUSE, &(_lightData->Specular.x));
-	glLightfv(GL_LIGHT0, GL_POSITION, &(_lightPosition->x));
+	glLightfv(GL_LIGHT0, GL_POSITION, &(_lightPosition->x));*/
 
 	for (int i = 0; i < 9; i++)
 	{
@@ -85,49 +85,49 @@ void HelloGL::InitObjects() {
 	//mercury
 	Mesh* mercuryMesh = MeshLoader::Load((char*)"cube.txt", MERCURY_SIZE);
 	Texture2D* mercuryTexture = new Texture2D();
-	mercuryTexture->Load((char*)"penguins.tga", 512, 512);
+	mercuryTexture->Load((char*)"Textures/mercury.bmp", 512, 512);
 	Planets[1] = new Planet(mercuryMesh, mercuryTexture, "Mercury", MERCURY_SIZE, MERCURY_DISTANCE, 0, 0, 88, MERCURY_DISTANCE);
 
 	//venus
 	Mesh* venusMesh = MeshLoader::Load((char*)"cube.txt", VENUS_SIZE);
 	Texture2D* venusTexture = new Texture2D();
-	venusTexture->Load((char*)"penguins.tga", 512, 512);
+	venusTexture->Load((char*)"Textures/venus.bmp", 512, 512);
 	Planets[2] = new Planet(venusMesh, venusTexture, "Venus", VENUS_SIZE, VENUS_DISTANCE, 0, 0, 225, VENUS_DISTANCE);
 
 	//earth
 	Mesh* earthMesh = MeshLoader::Load((char*)"cube.txt", EARTH_SIZE);
 	Texture2D* earthTexture = new Texture2D();
-	earthTexture->Load((char*)"penguins.tga", 512, 512);
+	earthTexture->Load((char*)"Textures/earth.bmp", 512, 512);
 	Planets[3] = new Planet(earthMesh, earthTexture, "Earth", EARTH_SIZE, EARTH_DISTANCE, 0, 0, 365, EARTH_DISTANCE);
 
 	//mars
 	Mesh* marsMesh = MeshLoader::Load((char*)"cube.txt", MARS_SIZE);
 	Texture2D* marsTexture = new Texture2D();
-	marsTexture->Load((char*)"penguins.tga", 512, 512);
+	marsTexture->Load((char*)"Textures/mars.bmp", 512, 512);
 	Planets[4] = new Planet(marsMesh, marsTexture, "Mars", MARS_SIZE, MARS_DISTANCE, 0, 0, 687, MARS_DISTANCE);
 
 	//jupiter
 	Mesh* jupiterMesh = MeshLoader::Load((char*)"cube.txt", JUPITER_SIZE);
 	Texture2D* jupiterTexture = new Texture2D();
-	jupiterTexture->Load((char*)"penguins.tga", 512, 512);
+	jupiterTexture->Load((char*)"Textures/jupiter.bmp", 512, 512);
 	Planets[5] = new Planet(jupiterMesh, jupiterTexture, "Jupiter", JUPITER_SIZE, JUPITER_DISTANCE, 0, 0, 4330, JUPITER_DISTANCE);
 
 	//saturn
 	Mesh* saturnMesh = MeshLoader::Load((char*)"cube.txt", SATURN_SIZE);
 	Texture2D* saturnTexture = new Texture2D();
-	saturnTexture->Load((char*)"penguins.raw", 512, 512);
+	saturnTexture->Load((char*)"Textures/saturn.bmp", 512, 512);
 	Planets[6] = new Planet(jupiterMesh, jupiterTexture, "Saturn", SATURN_SIZE, SATURN_DISTANCE, 0, 0, 10756, SATURN_DISTANCE);
 
 	//uranus
 	Mesh* uranusMesh = MeshLoader::Load((char*)"cube.txt", URANUS_SIZE);
 	Texture2D* uranusTexture = new Texture2D();
-	uranusTexture->Load((char*)"penguins.tga", 512, 512);
+	uranusTexture->Load((char*)"Textures/uranus.bmp", 512, 512);
 	Planets[7] = new Planet(uranusMesh, uranusTexture, "Uranus", URANUS_SIZE, URANUS_DISTANCE, 0, 0, 30687, URANUS_DISTANCE);
 
 	//neptune
 	Mesh* neptuneMesh = MeshLoader::Load((char*)"cube.txt", NEPTUNE_SIZE);
 	Texture2D* neptuneTexture = new Texture2D();
-	neptuneTexture->Load((char*)"penguins.tga", 512, 512);
+	neptuneTexture->Load((char*)"Textures/neptune.bmp", 512, 512);
 	Planets[8] = new Planet(neptuneMesh, neptuneTexture, "Neptune", NEPTUNE_SIZE, NEPTUNE_DISTANCE, 0, 0, 60190, NEPTUNE_DISTANCE);
 }
 
